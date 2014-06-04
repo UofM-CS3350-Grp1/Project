8705fd27ab6af6a979ce706027f5998b8ad9d7d4
Derek Chin <umchind@myumanitoba.ca>
2014-06-04 16:45:25 -0500

    Created scripts for swt jar files (32/64 bit)

---
+++ swt_jar_32-bit.bat
@@ -0,0 +1,6 @@
+@echo on
+
+erase .\lib\swt.jar
+copy .\lib\swt-3.6.2-win32-win32-x86.jar .\lib\swt.jar
+
+pause
---
+++ swt_jar_64-bit.bat
@@ -0,0 +1,6 @@
+@echo on
+
+erase .\lib\swt.jar
+copy .\lib\swt-3.6.2-win32-win32-x86_64.jar .\lib\swt.jar
+
+pause
