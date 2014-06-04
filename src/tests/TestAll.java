440d7ed63e34b6e704097e9e3baed099d1bc6949
Tim <steeltim@gmail.com>
2014-06-04 13:34:43 -0500

    Added some tests for the service class. Performed some error checking on the Service mutators.

--- src/tests/TestAll.java
+++ src/tests/TestAll.java
@@ -5,11 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import tests.objects.ClientTest;
+import tests.objects.ServiceTest;
 
 @RunWith(Suite.class)
 @SuiteClasses(
 		{
-			ClientTest.class
+			ClientTest.class,
+			ServiceTest.class
 		})
 public class TestAll
 { }
--- src/tests/objects/ServiceTest.java
+++ src/tests/objects/ServiceTest.java
@@ -1,14 +1,95 @@
 package tests.objects;
 
 import static org.junit.Assert.*;
-
+import objects.Service;
 import org.junit.Test;
 
-public class ServiceTest {
-
+/**
+ * Runs tests on the service class
+ */
+public class ServiceTest
+{		
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testService1()
+	{
+		System.out.println("\nStarting testService1:");
+		
+		Service service = new Service("Marketing", "Marketing Stuff", 10.0, "Type A");
+		
+		assertNotNull("Service is null", service);
+		assertTrue("Title is invalid", service.getTitle().equals("Marketing"));
+		assertTrue("Description name is invalid", service.getDescription().equals("Marketing Stuff"));
+		assertTrue("Type of service is invalid", service.getType().equals("Type A"));
+		assertTrue("Rate is invalid", service.getRate() == 10.0);
+		
+		System.out.println("\nFinished testService1");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testService2()
+	{
+		new Service(null, "Marketing Stuff", 10.0, "Type A");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testService3()
+	{
+		new Service("", "Marketing Stuff", 10.0, "Type A");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testService4()
+	{
+		new Service("Marketing", null, 10.0, "Type A");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testService5()
+	{
+		new Service("Marketing", "Marketing Stuff", 10.0, null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testService6()
+	{
+		new Service("Marketing", "Marketing Stuff", 10.0, "");
 	}
 
+	@Test(expected=IllegalArgumentException.class)
+	public void testService7()
+	{
+		new Service("Marketing", "Marketing Stuff", -1, "Type");
+	}
+	
+	@Test
+	public void testService8()
+	{
+		System.out.println("\nStarting testService8:");
+		
+		Service service = new Service("Marketing", "Marketing Stuff", 0.0, "Type A");
+		
+		assertNotNull("Service is null", service);
+		assertTrue("Title is invalid", service.getTitle().equals("Marketing"));
+		assertTrue("Description name is invalid", service.getDescription().equals("Marketing Stuff"));
+		assertTrue("Type of service is invalid", service.getType().equals("Type A"));
+		assertTrue("Rate is invalid", service.getRate() == 0.0);
+		
+		System.out.println("\nFinished testService8");
+	}
+	
+	@Test
+	public void testService9()
+	{
+		System.out.println("\nStarting testService9:");
+		
+		Service service = new Service("Marketing", "Marketing Stuff", 123465, "Type A");
+		
+		assertNotNull("Service is null", service);
+		assertTrue("Title is invalid", service.getTitle().equals("Marketing"));
+		assertTrue("Description name is invalid", service.getDescription().equals("Marketing Stuff"));
+		assertTrue("Type of service is invalid", service.getType().equals("Type A"));
+		assertTrue("Rate is invalid", service.getRate() == 123465);
+		
+		System.out.println("\nFinished testService9");
+	}
 }
