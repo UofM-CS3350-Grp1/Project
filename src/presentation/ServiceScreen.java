ef8edcaa9244785fcf487720173d4bf19d0db7a8
JasonTTT <jasontoews88@gmail.com>
2014-06-07 12:24:55 -0500

    Updated new/edit/delete processing
    Manipulation of the db is commented out at the moment

--- src/presentation/ServiceScreen.java
+++ src/presentation/ServiceScreen.java
@@ -11,10 +11,15 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import java.util.ArrayList;
 
 import objects.Service;
 
 import org.eclipse.swt.widgets.List;
+
+import persistence.DBController;
 
 /*
  * 
@@ -31,6 +36,13 @@ public class ServiceScreen extends Shell {
 	private Text svc_description;
 	private List list_services;
 	private Display display;
+	final Button svc_del = new Button(this, SWT.NONE);
+	final Button svc_edit = new Button(this, SWT.NONE);
+	final CCombo select_svc = new CCombo(this, SWT.BORDER);
+	Button btnCancel = new Button(this, SWT.NONE);
+	final Button btnAdd = new Button(this, SWT.NONE);
+	Button svc_new = new Button(this, SWT.NONE);
+	int selection = 0;
 
 	/**
 	 * Launch the application.
@@ -60,7 +72,7 @@ public class ServiceScreen extends Shell {
 		super(display, SWT.SHELL_TRIM);
 		
 		Label lblServiceType = new Label(this, SWT.NONE);
-		lblServiceType.setBounds(67, 95, 71, 15);
+		lblServiceType.setBounds(95, 145, 71, 15);
 		lblServiceType.setText("Service Type");
 		
 		Menu menu = new Menu(this, SWT.BAR);
@@ -71,76 +83,205 @@ public class ServiceScreen extends Shell {
 		lblAddNewService.setText("ADD / EDIT SERVICE");
 		
 		svcType = new Text(this, SWT.BORDER);
-		svcType.setBounds(186, 95, 263, 21);
+		svcType.setBounds(214, 145, 263, 21);
+		svcType.setEnabled(false);
 		
 		Label lblServiceRate = new Label(this, SWT.NONE);
-		lblServiceRate.setBounds(67, 130, 71, 15);
+		lblServiceRate.setBounds(95, 180, 71, 15);
 		lblServiceRate.setText("Service Rate");
 		
 		rate_amount = new Text(this, SWT.BORDER);
-		rate_amount.setBounds(186, 130, 76, 21);
+		rate_amount.setBounds(214, 180, 76, 21);
+		rate_amount.setEnabled(false);
 		
 		Label label = new Label(this, SWT.NONE);
-		label.setBounds(283, 136, 16, 15);
+		label.setBounds(311, 186, 16, 15);
 		label.setText("/");
 		
-		Combo rate_lenght = new Combo(this, SWT.NONE);
+		final Combo rate_lenght = new Combo(this, SWT.NONE);
 		rate_lenght.setItems(new String[] {"Year", "Month", "Week", "Session"});
-		rate_lenght.setBounds(305, 128, 91, 23);
-		
-		Label lblServiceTerms = new Label(this, SWT.NONE);
-		lblServiceTerms.setBounds(67, 166, 82, 15);
-		lblServiceTerms.setText("Service Terms");
-		
-		svc_terms = new Text(this, SWT.BORDER);
-		svc_terms.setBounds(186, 166, 263, 93);
+		rate_lenght.setBounds(333, 178, 91, 23);
+		rate_lenght.setEnabled(false);
 		
 		Label lblServiceDescription = new Label(this, SWT.NONE);
-		lblServiceDescription.setBounds(67, 275, 106, 15);
+		lblServiceDescription.setBounds(95, 222, 106, 15);
 		lblServiceDescription.setText("Service Description");
 		
 		svc_description = new Text(this, SWT.BORDER);
-		svc_description.setBounds(186, 275, 263, 93);
+		svc_description.setBounds(214, 219, 263, 141);
+		svc_description.setEnabled(false);
 		
-		Button btnAdd = new Button(this, SWT.NONE);
+		/*
+		 * Submit the selected options and fields - button event
+		 * the selection variable specifies if its an add, edit or delete
+		 */
 		btnAdd.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
+				String description = svc_description.toString();
+				String servicetype = svcType.toString();
+				Service service = new Service(servicetype, description, 100, servicetype);
+				submit_svc(service, selection);
 			}
 		});
 		btnAdd.setBounds(214, 391, 75, 25);
-		btnAdd.setText("ADD");
+		btnAdd.setText("SAVE");
+		btnAdd.setEnabled(false);
 		
-		Button btnCancel = new Button(this, SWT.NONE);
+		/*
+		 * Canceling the current selection - button event
+		 */
 		btnCancel.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent event) {
-				display.close();
+				clearFields();
+				svcType.setEnabled(false);
+				svc_description.setEnabled(false);
+				rate_amount.setEnabled(false);
+				rate_lenght.setEnabled(false);
+				select_svc.setEnabled(false);
+				svc_del.setEnabled(true);
+				svc_edit.setEnabled(true);
+				svc_new.setEnabled(true);
+				btnAdd.setEnabled(false);
+				selection = 0;
 			}
 		});
 		btnCancel.setBounds(348, 391, 75, 25);
 		btnCancel.setText("CANCEL");
 		
 		Label lblSelectService = new Label(this, SWT.NONE);
-		lblSelectService.setBounds(67, 63, 82, 15);
+		lblSelectService.setBounds(95, 113, 82, 15);
 		lblSelectService.setText("Select Service");
 
 		final Service svc = new Service();
 		
-		CCombo select_svc = new CCombo(this, SWT.BORDER);
 		select_svc.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
-				//int selectedIndex;
-				//selectedIndex = select_svc.getSelectionIndex();
 				editSelectedService(svc);
 			}
 		});
 
 		String test = svc.getTitle();
 		select_svc.setItems(new String[] {test});
-		select_svc.setBounds(186, 57, 168, 21);
+		select_svc.setBounds(214, 107, 168, 21);
+		select_svc.setEnabled(false);
+		
+		/*
+		 * Disables appropriate buttons and enables appropriate fields
+		 * for editing services - button event
+		 */
+		svc_edit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent arg0) {
+				svcType.setEnabled(true);
+				svc_description.setEnabled(true);
+				rate_amount.setEnabled(true);
+				rate_lenght.setEnabled(true);
+				select_svc.setEnabled(true);
+				svc_del.setEnabled(false);
+				svc_new.setEnabled(false);
+				svc_edit.setEnabled(true);
+				btnAdd.setEnabled(true);
+				btnAdd.setText("UPDATE");
+				selection = 2;
+			}
+		});
+		svc_edit.setBounds(262, 49, 75, 25);
+		svc_edit.setText("EDIT");
+		
+		/*
+		 * Disables appropriate buttons and enables appropriate fields
+		 * for editing services - button event
+		 */
+		svc_del.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent arg0) {
+				svcType.setEnabled(false);
+				svc_description.setEnabled(false);
+				rate_amount.setEnabled(false);
+				rate_lenght.setEnabled(false);
+				select_svc.setEnabled(true);
+				svc_del.setEnabled(false);
+				svc_edit.setEnabled(false);
+				svc_new.setEnabled(false);
+				btnAdd.setEnabled(true);
+				btnAdd.setText("DELETE");
+				selection = 3;
+			}
+		});
+		svc_del.setBounds(402, 49, 75, 25);
+		svc_del.setText("DELETE");
+		
+		/*
+		 * Disables appropriate buttons and enables appropriate fields
+		 * for adding a new service - button event
+		 */
+		svc_new.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent arg0) {
+				svcType.setEnabled(true);
+				svc_description.setEnabled(true);
+				rate_amount.setEnabled(true);
+				rate_lenght.setEnabled(true);
+				select_svc.setEnabled(false);
+				svc_del.setEnabled(false);
+				svc_edit.setEnabled(false);
+				btnAdd.setEnabled(true);
+				selection = 1;
+			}
+		});
+		svc_new.setBounds(126, 49, 75, 25);
+		svc_new.setText("NEW");
+		svc_new.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				clearFields();
+				
+			}
+		});
+		
 		createContents();
+	}
+
+	/*
+	 * Notes: This method submits the selected option and fields.
+	 * 
+	 * @param param - object containing the parameters of new/update/delete
+	 * @param selection - provides direction of what to do
+	 * 							- 1: add new service
+	 * 							- 2: edit a service
+	 * 							- 3: delete a service
+	 */
+	private void submit_svc(Object param, int selection) {
+		DBController db = new DBController("db");
+		db.connect();
+		Service temp = (Service)param;
+		
+		/*Remove comments once ready to use with the db*/
+		
+		/*
+		switch(selection){
+			case 1://add new
+				db.insert("SERVICES", temp);
+			case 2://edit
+				db.update("SERVICES", temp);
+			case 3://delete
+				db.drop("SERVICES", temp.getID());
+		}*/
+		
+		db.disconnect();
+	}
+	
+	/*
+	 * Notes: clears all input fields
+	 */
+	private void clearFields() {
+		svcType.setText("");
+		svc_description.setText("");
+		rate_amount.setText("");
+		
 	}
 	
 	private void editSelectedService(Service svc2){
