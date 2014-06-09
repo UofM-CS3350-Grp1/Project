9cf7c3c7553fc3c94322a3e8fb32558cf82ebf3d
Tim <steeltim@gmail.com>
2014-06-08 19:22:14 -0500

    Fixed error with running the tests.

--- setClasspath.bat
+++ setClasspath.bat
@@ -1,4 +1,4 @@
 @echo off
 
-REM set CLASSPATH=.;bin\;lib\atr.jar;lib\swt.jar;lib\junit.jar;lib\hsqldb.jar
-set CLASSPATH=.;.\bin\;.\lib\atr.jar;.\lib\hsqldb.jar;.\lib\junit.jar;.\lib\swt.jar
+REM set CLASSPATH=.;bin\;lib\atr.jar;lib\swt.jar;lib\junit.jar;lib\hsqldb.jar;lib\org.hamcrest.core_1.3.0.v201303031735.jar
+set CLASSPATH=.;.\bin\;.\lib\atr.jar;.\lib\hsqldb.jar;.\lib\junit.jar;.\lib\swt.jar;lib\org.hamcrest.core_1.3.0.v201303031735.jar
