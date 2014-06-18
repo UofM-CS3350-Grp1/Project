be22e4b03e1c40bef7cf4aa3e754df5cd3a28ee1
Tim <steeltim@gmail.com>
2014-06-18 02:07:58 -0500

    Work around for WindowBuilder limitation. See the constructor for details.

--- src/presentation/UpdateTrackableFeatureDrawer.java
+++ src/presentation/UpdateTrackableFeatureDrawer.java
@@ -26,80 +26,86 @@ public class UpdateTrackableFeatureDrawer
 	private ProcessAddFeature processAddFeature;
 	private TrackedFeature feature;
 	private GridData gd_txtNotes;
-	
+		
 	/**
 	 * Updates a given tracked feature
 	 * @param container 	The composite
-	 * @param feature		The feature to edit
+	 *
+	 * NOTE: Because the window builder will NOT recognize the constructor
+	 * or garbage error message 'no entry point found' if a second parameter
+	 * is set. So call setFeature() after construction
 	 */
-	public UpdateTrackableFeatureDrawer( Composite container, TrackedFeature feature ) throws IllegalArgumentException
+	public UpdateTrackableFeatureDrawer( Composite container ) 
 	{
-		assert (container != null && feature != null);
-		if(container != null && feature != null)
+		processAddFeature = new ProcessAddFeature();
+		
+		composite = new Composite( container, SWT.BORDER );
+				
+		// organizes the component
+		GridLayout compositeLayout = new GridLayout();
+		compositeLayout.numColumns = 2;
+		composite.setLayout( compositeLayout );
+		
+		// organizes how components look within composites
+		GridData componentTweaker = null;		
+		
+		// Name
+		Label lblName = new Label( composite, SWT.None );
+		lblName.setText( "Name" );
+		
+		txtName = new Text( composite, SWT.BORDER );
+		componentTweaker = new GridData( GridData.FILL_HORIZONTAL );
+		txtName.setLayoutData( componentTweaker );
+		
+		// Notes
+		Label lblNotes = new Label( composite, SWT.None );
+		lblNotes.setText( "Additional Details" );
+		new Label(composite, SWT.NONE);
+		
+		Text txtNotes = new Text( composite, SWT.BORDER );
+		gd_txtNotes = new GridData( GridData.FILL_BOTH );
+		gd_txtNotes.horizontalSpan = 2;
+		txtNotes.setLayoutData( gd_txtNotes );		
+		
+		buttonComposite = new Composite(composite, SWT.NONE);
+		GridData gd_buttonComposite = new GridData(SWT.CENTER, SWT.CENTER, false, false, 2, 1);
+		gd_buttonComposite.heightHint = 48;
+		gd_buttonComposite.widthHint = 155;
+		buttonComposite.setLayoutData(gd_buttonComposite);
+		
+		btnAction = new Button(buttonComposite, SWT.NONE);
+		btnAction.addSelectionListener(new SelectionAdapter() 
 		{
-			processAddFeature = new ProcessAddFeature();
+			@Override
+			public void widgetSelected(SelectionEvent event)
+			{
+				processActionButton();				
+			}
+		});
+		btnAction.setBounds(10, 10, 64, 25);
+		btnAction.setText("Update");
+		
+		btnCancel = new Button(buttonComposite, SWT.NONE);
+		btnCancel.addSelectionListener(new SelectionAdapter() 
+		{
+			@Override
+			public void widgetSelected(SelectionEvent event)
+			{
+				backToPreviousScreen();
+			}
+		});
+		btnCancel.setText("Cancel");
+		btnCancel.setBounds(80, 10, 64, 25);
+	}
+	
+	/**
+	 * @param feature the feature to set
+	 */
+	public void setFeature(TrackedFeature feature)
+	{
+		assert (feature != null);
+		if(feature != null)
 			this.feature = feature;
-			
-			composite = new Composite( container, SWT.BORDER );
-					
-			// organizes the component
-			GridLayout compositeLayout = new GridLayout();
-			compositeLayout.numColumns = 2;
-			composite.setLayout( compositeLayout );
-			
-			// organizes how components look within composites
-			GridData componentTweaker = null;		
-			
-			// Name
-			Label lblName = new Label( composite, SWT.None );
-			lblName.setText( "Name" );
-			
-			txtName = new Text( composite, SWT.BORDER );
-			componentTweaker = new GridData( GridData.FILL_HORIZONTAL );
-			txtName.setLayoutData( componentTweaker );
-			
-			// Notes
-			Label lblNotes = new Label( composite, SWT.None );
-			lblNotes.setText( "Additional Details" );
-			new Label(composite, SWT.NONE);
-			
-			Text txtNotes = new Text( composite, SWT.BORDER );
-			gd_txtNotes = new GridData( GridData.FILL_BOTH );
-			gd_txtNotes.horizontalSpan = 2;
-			txtNotes.setLayoutData( gd_txtNotes );		
-			
-			buttonComposite = new Composite(composite, SWT.NONE);
-			GridData gd_buttonComposite = new GridData(SWT.CENTER, SWT.CENTER, false, false, 2, 1);
-			gd_buttonComposite.heightHint = 48;
-			gd_buttonComposite.widthHint = 155;
-			buttonComposite.setLayoutData(gd_buttonComposite);
-			
-			btnAction = new Button(buttonComposite, SWT.NONE);
-			btnAction.addSelectionListener(new SelectionAdapter() 
-			{
-				@Override
-				public void widgetSelected(SelectionEvent event)
-				{
-					processActionButton();				
-				}
-			});
-			btnAction.setBounds(10, 10, 64, 25);
-			btnAction.setText("Update");
-			
-			btnCancel = new Button(buttonComposite, SWT.NONE);
-			btnCancel.addSelectionListener(new SelectionAdapter() 
-			{
-				@Override
-				public void widgetSelected(SelectionEvent event)
-				{
-					backToPreviousScreen();
-				}
-			});
-			btnCancel.setText("Cancel");
-			btnCancel.setBounds(80, 10, 64, 25);
-		}
-		else
-			throw new IllegalArgumentException();
 	}
 	
 	/**
@@ -107,11 +113,14 @@ public class UpdateTrackableFeatureDrawer
 	 */
 	protected void processActionButton()
 	{		
-		feature.setFeatureName(txtName.getText());
-		feature.setNotes(txtNotes.getText());
-		
-		processAddFeature.updateFeature(feature);
-		backToPreviousScreen();
+		if(feature != null)
+		{
+			feature.setFeatureName(txtName.getText());
+			feature.setNotes(txtNotes.getText());
+			
+			processAddFeature.updateFeature(feature);
+			backToPreviousScreen();
+		}
 	}
 	
 	/**
