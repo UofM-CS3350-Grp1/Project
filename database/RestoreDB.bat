c3c25630232759a743466f31a911619a332e2d67
Derek Chin <umchind@myumanitoba.ca>
2014-06-25 14:58:39 -0500

    Removed old RestoreDB.bat file, moved output formatting to new one

--- database/RestoreDB.bat
+++ database/RestoreDB.bat
@@ -1,7 +1,14 @@
+@echo on
+
+echo.
+echo Restoring database to initial configuration...
+
 copy DatabaseBackups\MainDB.script MainDB.script
 copy DatabaseBackups\CacheDB.script CacheDB.script
 copy DatabaseBackups\MainDB.properties MainDB.properties
 copy DatabaseBackups\CacheDB.properties CacheDB.properties
 copy DatabaseBackups\CacheDB.data CacheDB.data
 
-REM pause
+echo.
+echo Database restored.
+pause
