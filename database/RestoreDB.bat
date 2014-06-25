f5ea388c70d3f1ee382367b977303d46467606cd
Adrian Harrison <umharr68@myumanitoba.ca>
2014-06-24 22:52:59 -0500

    Added Backup Batch File.

---
+++ database/RestoreDB.bat
@@ -0,0 +1,7 @@
+copy DatabaseBackups\MainDB.script MainDB.script
+copy DatabaseBackups\CacheDB.script CacheDB.script
+copy DatabaseBackups\MainDB.properties MainDB.properties
+copy DatabaseBackups\CacheDB.properties CacheDB.properties
+copy DatabaseBackups\CacheDB.data CacheDB.data
+
+REM pause
