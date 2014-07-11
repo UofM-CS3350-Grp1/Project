9c6424955a626a7b013349bb6ec4ac844926823e
Jason <jasontoews88@gmail.com>
2014-07-11 17:07:49 -0500

    Updated TestIntegration class to run all integration tests

--- src/tests/TestIntegration.java
+++ src/tests/TestIntegration.java
@@ -4,6 +4,19 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import tests.integration.business.TestAccessFinancialRecords;
+import tests.integration.business.TestCalculateFeatureValue;
+import tests.integration.business.TestDateTimeUtil;
+import tests.integration.business.TestGenerateGraph;
+import tests.integration.business.TestProcessAddFeature;
+import tests.integration.business.TestProcessClient;
+import tests.integration.business.TestProcessContract;
+import tests.integration.business.TestProcessExpenses;
+import tests.integration.business.TestProcessFeatureHistory;
+import tests.integration.business.TestProcessService;
+import tests.integration.business.TestProcessStorable;
+import tests.integration.business.TestProcessUser;
+import tests.integration.business.TestValidateTextbox;
 import tests.integration.persistence.TestDBController;
 import tests.integration.persistence.TestDBInterface;
 
@@ -12,6 +25,19 @@ import tests.integration.persistence.TestDBInterface;
 	
 	TestDBController.class,
 	TestDBInterface.class,
+	TestAccessFinancialRecords.class,
+	TestCalculateFeatureValue.class,
+	TestDateTimeUtil.class,
+	TestGenerateGraph.class,
+	TestProcessAddFeature.class,
+	TestProcessClient.class,
+	TestProcessContract.class,
+	TestProcessExpenses.class,
+	TestProcessFeatureHistory.class,
+	TestProcessService.class,
+	TestProcessStorable.class,
+	TestProcessUser.class,
+	TestValidateTextbox.class,
 })
 public class TestIntegration {
 
