c2052c115d95d53b03c002850793461a86b4225e
Jason <jasontoews88@gmail.com>
2014-07-12 00:43:03 -0500

    Change to gui. Original code commented out.

--- src/presentation/SwitchScreen.java
+++ src/presentation/SwitchScreen.java
@@ -16,12 +16,18 @@ import org.eclipse.swt.events.SelectionEvent;
 import presentation.ClientScreenDrawer;
 import presentation.ServiceScreenDrawer;
 import objects.User;
-
 import acceptanceTests.Register;
 import acceptanceTests.EventLoop;
 
 import java.util.Deque;
 import java.util.ArrayDeque;
+
+import javax.imageio.ImageIO;
+
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Label;
 
 public class SwitchScreen
 {
@@ -45,68 +51,74 @@ public class SwitchScreen
 	private static Button bExit;
 	private static Display display;
 	private static Shell shell;
+	private static MenuItem mntmMenu;
+	private static Menu menu;
 
 	SwitchScreen()
 	{	
 		backStack = new ArrayDeque< Composite >();
 
 		display = Display.getDefault();
-		shell = new Shell();
+		shell = new Shell(display);
 		initShell( shell );
 		Register.newWindow(this);
+		shell.setMaximized(true);
 
 		/*
 		 * Create the navigation bar
 		 */
-		final int NUM_NAV_BUTTONS = 8;
+		/*final int NUM_NAV_BUTTONS = 8;
 		Composite navBar = new Composite( shell, SWT.BORDER );
 		GridLayout navLayout = new GridLayout();
-		navLayout.numColumns = NUM_NAV_BUTTONS;
+		navLayout.numColumns = 8;
 		navLayout.makeColumnsEqualWidth = true;
 		navBar.setLayout( navLayout );
-		navBar.setLayoutData( new GridData( GridData.FILL_HORIZONTAL ) );
+		GridData gd_navBar = new GridData( GridData.FILL_HORIZONTAL );
+		gd_navBar.heightHint = 78;
+		navBar.setLayoutData( gd_navBar );*/
+		
+		//bBack = new Button( navBar, SWT.FLAT );
+		//tuneNavButton( bBack, "BACK" );
 
-		/*
-		 * adds the buttons to the nav bar; more than six will cause it to overflow
-		 * to a new row; if you need more buttons, adjust num columns above
-		 */
-		bBack = new Button( navBar, SWT.FLAT );
-		tuneNavButton( bBack, "BACK" );
+		//bClients = new Button( navBar, SWT.FLAT );
+		//tuneNavButton( bClients, "CLIENTS" );
 
-		bClients = new Button( navBar, SWT.FLAT );
-		tuneNavButton( bClients, "CLIENTS" );
+		//bContract = new Button( navBar, SWT.FLAT );
+		//tuneNavButton( bContract, "CONTRACTS" );
 
-		bContract = new Button( navBar, SWT.FLAT );
-		tuneNavButton( bContract, "CONTRACTS" );
+		//bServices = new Button( navBar, SWT.FLAT );
+		//tuneNavButton( bServices, "SERVICES" );
 
-		bServices = new Button( navBar, SWT.FLAT );
-		tuneNavButton( bServices, "SERVICES" );
+		//bFeatures = new Button( navBar, SWT.FLAT );
+		//tuneNavButton( bFeatures, "FEATURES" );
 
-		bFeatures = new Button( navBar, SWT.FLAT );
-		tuneNavButton( bFeatures, "FEATURES" );
+		//bJCC = new Button( navBar, SWT.FLAT );
+		//tuneNavButton( bJCC, "JCC's" );
 
-		bJCC = new Button( navBar, SWT.FLAT );
-		tuneNavButton( bJCC, "JCC's" );
+		//bLogin = new Button( navBar, SWT.FLAT );
+		//tuneNavButton( bLogin, "LOG IN" );
 
-		bLogin = new Button( navBar, SWT.FLAT );
-		tuneNavButton( bLogin, "LOG IN" );
-
-		bExit = new Button( navBar, SWT.FLAT );
-		tuneNavButton( bExit, "EXIT" );
+		//bExit = new Button( navBar, SWT.FLAT );
+		//tuneNavButton( bExit, "EXIT" );
+		/*new Label(navBar, SWT.NONE);
+		new Label(navBar, SWT.NONE);
+		new Label(navBar, SWT.NONE);
+		new Label(navBar, SWT.NONE);
+		new Label(navBar, SWT.NONE);*/
 
 		disableButtons();
 
 		/*
 		 * Back button listener
 		 */
-		bBack.addSelectionListener( new SelectionAdapter()
+		/*bBack.addSelectionListener( new SelectionAdapter()
 		{
 			@Override
 			public void widgetSelected( SelectionEvent event )
 			{
 				switchBack();
 			}
-		});
+		});*/
 
 		/*
 		 * create the switching composite
@@ -115,6 +127,204 @@ public class SwitchScreen
 		content.setLayoutData( new GridData( GridData.FILL_BOTH ) );
 		contentLayout = new StackLayout(); // allows switching between composites
 		content.setLayout( contentLayout );
+
+/*
+ * This is the added code for the top menu bar
+ */
+		menu = new Menu(shell, SWT.BAR);
+		shell.setMenuBar(menu);
+		
+		mntmMenu = new MenuItem(menu, SWT.CASCADE);
+		mntmMenu.setText("Menu");
+		
+		Menu menu_1 = new Menu(mntmMenu);
+		mntmMenu.setMenu(menu_1);
+		
+		MenuItem bClients = new MenuItem(menu_1, SWT.NONE);
+		bClients.setText("Clients");
+		
+		MenuItem bContract = new MenuItem(menu_1, SWT.NONE);
+		bContract.setText("Contracts");
+		
+		
+		MenuItem bServices = new MenuItem(menu_1, SWT.NONE);
+		bServices.setText("Services");
+		
+		MenuItem bFeatures = new MenuItem(menu_1, SWT.NONE);
+		bFeatures.setText("Features");
+		
+		MenuItem bJCC = new MenuItem(menu_1, SWT.NONE);
+		bJCC.setText("JCC's");
+		
+		new MenuItem(menu_1, SWT.SEPARATOR);
+		
+		MenuItem mntmLogOut = new MenuItem(menu_1, SWT.NONE);
+		mntmLogOut.setText("Log Out");
+		
+		MenuItem bExit = new MenuItem(menu_1, SWT.NONE);
+		bExit.setText("Exit");
+		
+		MenuItem mntmNewSubmenu = new MenuItem(menu, SWT.CASCADE);
+		mntmNewSubmenu.setText("New");
+		
+		Menu menu_2 = new Menu(mntmNewSubmenu);
+		mntmNewSubmenu.setMenu(menu_2);
+		
+		MenuItem mntmNewClient = new MenuItem(menu_2, SWT.NONE);
+		mntmNewClient.setText("New Client");
+
+		mntmNewClient.addSelectionListener( new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected( SelectionEvent event )
+			{
+				Composite addClientScreen = SwitchScreen.getContentContainer();
+				new AddClientScreenDrawer( addClientScreen );
+				SwitchScreen.switchContent( addClientScreen );
+			}
+		});
+		
+		MenuItem mntmNewContract = new MenuItem(menu_2, SWT.NONE);
+		mntmNewContract.setText("New Contract");
+
+		mntmNewContract.addSelectionListener( new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected( SelectionEvent event )
+			{
+				Composite addContractScreen = SwitchScreen.getContentContainer();
+				new AddContractScreenDrawer( addContractScreen );
+				SwitchScreen.switchContent( addContractScreen );
+			}
+		});
+		
+		MenuItem mntmNewService = new MenuItem(menu_2, SWT.NONE);
+		mntmNewService.setText("New Service");
+
+		mntmNewService.addSelectionListener( new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected( SelectionEvent event )
+			{
+				Composite addServiceScreen = SwitchScreen.getContentContainer();
+				new AddServiceScreenDrawer( addServiceScreen );
+				SwitchScreen.switchContent( addServiceScreen );
+			}
+		});
+		
+		new MenuItem(menu_2, SWT.SEPARATOR);
+		
+		MenuItem mntmNewFeature = new MenuItem(menu_2, SWT.NONE);
+		mntmNewFeature.setText("New Feature");
+
+		mntmNewFeature.addSelectionListener( new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected( SelectionEvent event )
+			{
+				Composite addFeatureScreen = SwitchScreen.getContentContainer();
+				new AddTrackableFeatureDrawer( addFeatureScreen );
+				SwitchScreen.switchContent( addFeatureScreen );
+			}
+		});
+		
+		MenuItem mntmNewSurvey = new MenuItem(menu_2, SWT.NONE);
+		mntmNewSurvey.setText("New Survey");
+
+		mntmNewSurvey.addSelectionListener( new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected( SelectionEvent event )
+			{
+				Composite addSurveyScreen = SwitchScreen.getContentContainer();
+				new JCCSurveyScreenDrawer( addSurveyScreen );
+				SwitchScreen.switchContent( addSurveyScreen );
+			}
+		});
+		
+		MenuItem mntmJccs_1 = new MenuItem(menu, SWT.CASCADE);
+		mntmJccs_1.setText("JCC's");
+		
+		Menu menu_3 = new Menu(mntmJccs_1);
+		mntmJccs_1.setMenu(menu_3);
+		
+		MenuItem mntmViewContracts = new MenuItem(menu_3, SWT.NONE);
+		mntmViewContracts.setText("View Contracts");
+
+		mntmViewContracts.addSelectionListener( new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected( SelectionEvent event )
+			{
+				Composite viewJCCContractScreen = SwitchScreen.getContentContainer();
+				new JCCContractScreenDrawer( viewJCCContractScreen );
+				SwitchScreen.switchContent( viewJCCContractScreen );
+			}
+		});
+		
+		MenuItem mntmViewServices = new MenuItem(menu_3, SWT.NONE);
+		mntmViewServices.setText("View Services");
+
+		mntmViewServices.addSelectionListener( new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected( SelectionEvent event )
+			{
+				Composite viewJCCServiceScreen = SwitchScreen.getContentContainer();
+				new JCCServiceScreenDrawer( viewJCCServiceScreen );
+				SwitchScreen.switchContent( viewJCCServiceScreen );
+			}
+		});
+		
+		MenuItem mntmViewClients = new MenuItem(menu_3, SWT.NONE);
+		mntmViewClients.setText("View Clients");
+
+		mntmViewClients.addSelectionListener( new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected( SelectionEvent event )
+			{
+				Composite viewJCCClientScreen = SwitchScreen.getContentContainer();
+				new JCCClientScreenDrawer( viewJCCClientScreen );
+				SwitchScreen.switchContent( viewJCCClientScreen );
+			}
+		});
+		
+		new MenuItem(menu_3, SWT.SEPARATOR);
+		
+		MenuItem mntmAddExpenses = new MenuItem(menu_3, SWT.NONE);
+		mntmAddExpenses.setText("Add Expenses");
+
+		mntmAddExpenses.addSelectionListener( new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected( SelectionEvent event )
+			{
+				Composite addExpensesScreen = SwitchScreen.getContentContainer();
+				new AddExpensesScreenDrawer( addExpensesScreen );
+				SwitchScreen.switchContent( addExpensesScreen );
+			}
+		});
+		
+		MenuItem mntmSettings = new MenuItem(menu, SWT.CASCADE);
+		mntmSettings.setText("Settings");
+		
+		Menu menu_4 = new Menu(mntmSettings);
+		mntmSettings.setMenu(menu_4);
+		
+		MenuItem mntmChangePassword = new MenuItem(menu_4, SWT.NONE);
+		mntmChangePassword.setText("Change Password");
+		
+		new MenuItem(menu_4, SWT.SEPARATOR);
+		
+		MenuItem bLogin = new MenuItem(menu_4, SWT.NONE);
+		bLogin.setText("Log In");
+		
+		MenuItem mntmLogOut_1 = new MenuItem(menu_4, SWT.NONE);
+		mntmLogOut_1.setText("Log Out");
+/*
+ * Top menu bar code ends here
+ */
 
 		/*
 		 *  draws the contracts screen
@@ -236,7 +446,8 @@ public class SwitchScreen
 			@Override
 			public void widgetSelected(SelectionEvent event) 
 			{
-				((Control) event.getSource()).getShell().dispose();
+				//((Control) event.getSource()).getShell().dispose();
+				System.exit(0);
 			}
 		});
 				
@@ -323,22 +534,22 @@ public class SwitchScreen
 
 	public static void disableButtons()
 	{
-		bBack.setEnabled(false);
+		/*bBack.setEnabled(false);
 		bClients.setEnabled(false);
 		bContract.setEnabled(false);
 		bServices.setEnabled(false);
 		bFeatures.setEnabled(false);
-		bJCC.setEnabled(false);
+		bJCC.setEnabled(false);*/
 	}
 
 	public static void enableButtons()
 	{
-		bBack.setEnabled(true);
+		/*bBack.setEnabled(true);
 		bClients.setEnabled(true);
 		bContract.setEnabled(true);
 		bServices.setEnabled(true);
 		bFeatures.setEnabled(true);
-		bJCC.setEnabled(true);
+		bJCC.setEnabled(true);*/
 	}
 
 	/**
