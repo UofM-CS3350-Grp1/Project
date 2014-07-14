97c6e5c1f9145b48f29eeee2bb04461161aec150
Derek Chin <umchind@myumanitoba.ca>
2014-07-14 00:47:05 -0500

    Corrected ProcessFeatureHistory integration tests

--- src/tests/integration/business/ProcessFeatureHistoryTest.java
+++ src/tests/integration/business/ProcessFeatureHistoryTest.java
@@ -3,10 +3,13 @@ package tests.integration.business;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.junit.Test;
 
 import business.ProcessFeatureHistory;
+import business.ProcessClient;
+import business.ProcessAddFeature;
 import objects.Client;
 import objects.Email;
 import objects.MonthReport;
@@ -18,68 +21,151 @@ import objects.Client.ClientStatus;
 public class ProcessFeatureHistoryTest
 {
 	private ProcessFeatureHistory processFeatureHistory; 
+	private ProcessClient processClient;
+	private ProcessAddFeature processAddFeature;
 
 	public ProcessFeatureHistoryTest()
 	{
 		processFeatureHistory = new ProcessFeatureHistory();
+		processClient = new ProcessClient();
+		processAddFeature = new ProcessAddFeature(); 
 	}
 
 	@Test
 	public void getFeatureTotalTest()
 	{
-		Email email = new Email("test@test.com");
+		double value = -14;
+		Email email = new Email("testmail@testing.com");
 		PhoneNumber phone = new PhoneNumber("2049601538");
-		Client newClient = new Client("Jason", phone, email, "123 First st", "BusinessNew1", ClientStatus.Active);
+		Client newClient = new Client("Billy", phone, email, "123 FirstLast St", "BusinessShoe1", ClientStatus.Active);
+		assertTrue(processClient.insert(newClient));
 
-		TrackedFeatureType newFeature = new TrackedFeatureType("Features");
+		TrackedFeatureType newFeature = new TrackedFeatureType("myFeature");
+		assertTrue(processAddFeature.insert(newFeature));
+
+		Client theClient = processClient.getClientByBusinessName("BusinessShoe1");
+		assertNotNull(theClient);
+
+		ArrayList<TrackedFeatureType> featureTypes = processAddFeature.getFeatureTypes();
+		assertNotNull(featureTypes);
+
+		TrackedFeatureType theFeature = null;
+		for (int i = 0; i < featureTypes.size() && theFeature == null; i++)
+		{
+			if (featureTypes.get(i).getTitle().equals("myFeature"))
+				theFeature = featureTypes.get(i);
+		}
 
 		processFeatureHistory = new ProcessFeatureHistory();
-		double value = processFeatureHistory.getFeatureTotal(newClient, newFeature);
+		value = processFeatureHistory.getFeatureTotal(theClient, newFeature);
 
-		assertNotNull(value);
+		assertNotEquals(-14,value);
+
+		processAddFeature.delete(newFeature);
+		processClient.delete(newClient);		
 	}
 
 	@Test
 	public void getHistoryByIDTest()
 	{
-		TrackedFeature feature;
-		feature = processFeatureHistory.getHistoryByID(-1);
-		assertEquals(null,feature);
+		Email email = new Email("testmail@testing.com");
+		PhoneNumber phone = new PhoneNumber("2049601538");
+		Client newClient = new Client("Willy", phone, email, "123 FirstLast St", "BusinessHotel", ClientStatus.Active);
+		assertTrue(processClient.insert(newClient));
+
+		TrackedFeatureType newFeatureType = new TrackedFeatureType("myFeature");
+		assertTrue(processAddFeature.insert(newFeatureType));
+
+		ArrayList<TrackedFeatureType> featureTypes = processAddFeature.getFeatureTypes();
+		assertNotNull(featureTypes);
+
+		TrackedFeatureType myFeatureType = null;
+		for (int i = 0; i < featureTypes.size() && myFeatureType == null; i++)
+		{
+			if (featureTypes.get(i).getTitle().equals("myFeature"))
+				myFeatureType = featureTypes.get(i);
+		}
+		
+		Client theClient = processClient.getClientByBusinessName("BusinessHotel");
+		assertNotNull(theClient);
+
+		TrackedFeature theFeature = new TrackedFeature("notes", theClient.getID(), myFeatureType, new Date(1402194845), 1.5);
+		assertNotNull(theFeature);
+
+		assertTrue(processAddFeature.insert(theFeature));
+		TrackedFeature aFeature = processFeatureHistory.getNextHistory();
+		assertNotNull(aFeature);
+
+		TrackedFeature feature = processFeatureHistory.getHistoryByID(aFeature.getID());
+		assertNotNull(feature);
+
+		processClient.delete(newClient);
+		processAddFeature.delete(newFeatureType);
+		processAddFeature.delete(theFeature);
 	}
 
 	@Test
 	public void getNextHistoryTest() 
 	{
-		TrackedFeature feature;
-		feature = processFeatureHistory.getNextHistory();
-		assertNotNull(feature);
+		Email email = new Email("testmail@testingg.com");
+		PhoneNumber phone = new PhoneNumber("2041960158");
+		Client newClient = new Client("Will", phone, email, "1243 First Last St", "Submarines", ClientStatus.Active);
+		assertTrue(processClient.insert(newClient));
+
+		TrackedFeatureType newFeatureType = new TrackedFeatureType("myFeature");
+		assertTrue(processAddFeature.insert(newFeatureType));
+
+		ArrayList<TrackedFeatureType> featureTypes = processAddFeature.getFeatureTypes();
+		assertNotNull(featureTypes);
+
+		TrackedFeatureType myFeatureType = null;
+		for (int i = 0; i < featureTypes.size() && myFeatureType == null; i++)
+		{
+			if (featureTypes.get(i).getTitle().equals("myFeature"))
+				myFeatureType = featureTypes.get(i);
+		}
+		
+		Client theClient = processClient.getClientByBusinessName("Submarines");
+		assertNotNull(theClient);
+
+		TrackedFeature theFeature = new TrackedFeature("notes", theClient.getID(), myFeatureType, new Date(1402194845), 1.5);
+		assertNotNull(theFeature);
+
+		assertTrue(processAddFeature.insert(theFeature));
+		TrackedFeature aFeature = processFeatureHistory.getNextHistory();
+		assertNotNull(aFeature);
+
+		processClient.delete(newClient);
+		processAddFeature.delete(newFeatureType);
+		processAddFeature.delete(theFeature);
 	}
 
 	@Test
-	public void getYearHistoryForFeatureTest1()
-	{
-		ArrayList<MonthReport> reports;
-		reports = processFeatureHistory.getYearHistoryForFeature(null, null);
-		assertNotNull(reports);
-	}
-
-	public void getYearHistoryForFeatureTest2()
+	public void getYearHistoryForFeatureTest()
 	{
 		ArrayList<MonthReport> reports;
 
-		Email email = new Email("test@test.com");
-		PhoneNumber phone = new PhoneNumber("2049601538");
-		Client newClient = new Client("Jason", phone, email, "123 First st", "BusinessNew1", ClientStatus.Active);
+		Email email = new Email("testhail@testing.com");
+		PhoneNumber phone = new PhoneNumber("2044601538");
+		Client newClient = new Client("Sammy", phone, email, "1223 FirstLast St", "BusinessMotel", ClientStatus.Active);
+		assertTrue(processClient.insert(newClient));
 
-		reports = processFeatureHistory.getYearHistoryForFeature(null, newClient);
-		assertNotNull(reports);
-	}
+		TrackedFeatureType newFeatureType = new TrackedFeatureType("myFeature");
+		assertTrue(processAddFeature.insert(newFeatureType));
 
-	public void getYearHistoryForFeatureTest3()
-	{
-		ArrayList<MonthReport> reports;
-		TrackedFeatureType featureType = new TrackedFeatureType("Features");
-		reports = processFeatureHistory.getYearHistoryForFeature(featureType, null);
-		assertNotNull(reports);
+		ArrayList<TrackedFeatureType> featureTypes = processAddFeature.getFeatureTypes();
+		assertNotNull(featureTypes);
+
+		TrackedFeatureType myFeatureType = null;
+		for (int i = 0; i < featureTypes.size() && myFeatureType == null; i++)
+		{
+			if (featureTypes.get(i).getTitle().equals("myFeature"))
+				myFeatureType = featureTypes.get(i);
+		}
+
+		reports = processFeatureHistory.getYearHistoryForFeature(myFeatureType, newClient);
+
+		processClient.delete(newClient);
+		processAddFeature.delete(newFeatureType);
 	}
 }
