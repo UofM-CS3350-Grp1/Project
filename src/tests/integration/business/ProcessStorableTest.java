c764fed98809be8a1da9fb6406c636cdacf489f3
Tim <steeltim@gmail.com>
2014-07-14 02:29:20 -0500

    Added test information.

--- src/tests/integration/business/ProcessStorableTest.java
+++ src/tests/integration/business/ProcessStorableTest.java
@@ -2,20 +2,31 @@ package tests.integration.business;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
-import persistence.DBInterface;
 import business.ProcessStorable;
 
-public class ProcessStorableTest {
-
-	private DBInterface database;
-	private ProcessStorable processStorable; 
-
-	public ProcessStorableTest()
+public class ProcessStorableTest
+{
+	private ProcessStorable processStorable = new ProcessStorable();
+	
+	@Rule
+	public TestName testName = new TestName();
+	
+	@Before
+	public void before()
 	{
-		processStorable = new ProcessStorable();
-		database = new DBInterface("db");
+		System.out.println("Running test: " + this.getClass().toString() + "::" + testName.getMethodName());
+	}
+	
+	@After
+	public void after()
+	{
+		System.out.println("Finished test.\n");
 	}
 
 	@Test
