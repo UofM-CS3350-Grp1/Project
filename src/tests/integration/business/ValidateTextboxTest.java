ff01ec80441f33e96b622e121b4cc933bd103c3d
Tim <steeltim@gmail.com>
2014-07-14 02:47:19 -0500

    Added test information.

--- src/tests/integration/business/ValidateTextboxTest.java
+++ src/tests/integration/business/ValidateTextboxTest.java
@@ -1,11 +1,30 @@
 package tests.integration.business;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
 import business.ValidateTextbox;
 
 public class ValidateTextboxTest
 {
+	@Rule
+	public TestName testName = new TestName();
+	
+	@Before
+	public void before()
+	{
+		System.out.println("Running test: " + this.getClass().toString() + "::" + testName.getMethodName());
+	}
+	
+	@After
+	public void after()
+	{
+		System.out.println("Finished test.\n");
+	}
+	
 	@Test
 	public void verifyMonetaryValueTest()
 	{
