6845ed5076e779834a6689ec05acf0a0f9e76448
Tim <steeltim@gmail.com>
2014-07-13 22:49:55 -0500

    Added integration test for AccessFinancialRecords. Still a few quirks.

--- src/tests/integration/business/TestAccessFinancialRecords.java
+++ src/tests/integration/business/TestAccessFinancialRecords.java
@@ -3,121 +3,176 @@ package tests.integration.business;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import objects.Client;
 import objects.Client.ClientStatus;
 import objects.Contract;
 import objects.Email;
+import objects.Expense;
 import objects.MonthReport;
 import objects.PhoneNumber;
 import objects.Service;
 import objects.ServiceType;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import persistence.DBInterface;
 import business.AccessFinancialRecords;
 import business.ProcessClient;
 import business.ProcessContract;
+import business.ProcessExpenses;
 import business.ProcessService;
 
-public class TestAccessFinancialRecords {
-
-	DBInterface database = new DBInterface("db");
-	AccessFinancialRecords financialRecords;
+public class TestAccessFinancialRecords
+{
+	private AccessFinancialRecords financialRecords;
+	private ProcessClient processClient;
+	private ProcessService processService;
+	private ProcessContract processContract;
+	private ServiceType serviceType;
+	private Contract contract;
+	private Client client;
+	private Service service;
+	private Email email;
+	private PhoneNumber phone;
+	
+	@Before
+	public void before()
+	{
+		boolean found = false;
+		
+		financialRecords = new AccessFinancialRecords();
+		processClient = new ProcessClient();
+		processService = new ProcessService();
+		processContract = new ProcessContract();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.MONTH, -1);
+		serviceType = new ServiceType("Type", "Description");
+		contract = new Contract(processContract.getUnusedContractID(), "Biz", "details", 50.0, new Date(), cal.getTime(), cal.getTime());
+		phone = new PhoneNumber("2049601538");
+		email = new Email("test@hotmail.com");
+		client = new Client("Client", phone, email, "123 First st", "Biz", ClientStatus.Active);
+		
+		assertTrue("Service type not inserted", processService.insert(serviceType));
+		assertTrue("Client not inserted", processClient.insert(client));
+		assertTrue("Contract not inserted", processContract.insert(contract));
+		
+		ArrayList<ServiceType> types = processService.getServiceTypes();
+		for(int i = 0; i < types.size() && !found; i++)
+		{
+			if(types.get(i).getType().equals(serviceType.getType()))
+			{
+				serviceType = types.get(i);
+				found = true;
+			}
+		}
+		
+		client = processClient.getClientByBusinessName("Biz");
+		assertNotNull("Client is null", client);
+		
+		service = new Service("Title", "Description", 50.0, serviceType);		
+		service.setClientID(client.getID());
+		service.setContractID(contract.getID());		
+		assertTrue("Service not inserted", processService.insert(service));
+		service = processService.getServiceByTitle("Title");
+		assertNotNull("Service is null", service);
+	}
+	
+	@After
+	public void after()
+	{
+		assertTrue("Contract not deleted", processContract.delete(contract));
+		assertTrue("Service not deleted", processService.delete(service));
+		assertTrue("Client not deleted", processClient.delete(client));
+		assertTrue("Service type not deleted", processService.delete(serviceType));
+	}
 	
 	@Test
-	public void testAccessFinancialRecords() {
-		financialRecords = new AccessFinancialRecords();
+	public void testAccessFinancialRecords() 
+	{
 		assertNotNull(financialRecords);
 	}
 
 	@Test
-	public void testGetYearRevenueForClient() {
-		
-		financialRecords = new AccessFinancialRecords();
-		ServiceType serviceType = new ServiceType("Type", "Description");
-		Service service = new Service("Title", "Description", 50.0, serviceType);
-		Contract contract = new Contract("Biz", "details", 50.0, new Date(), new Date(), new Date());
-		PhoneNumber phone = new PhoneNumber("2049601538");
-		Email email = new Email("test@hotmail.com");
-		Client client = new Client("Client", phone, email, "123 First st", "Biz", ClientStatus.Active);
-		ProcessClient processClient = new ProcessClient();
-		ProcessService processService = new ProcessService();
-		ProcessContract processContract = new ProcessContract();
-
-		database.connect();
-		processContract.insert(contract);
-		processService.insert(serviceType);
-		processClient.insert(client);
-		database.disconnect();
-		
-		client = processClient.getClientByBusinessName("Biz");
-		ArrayList<Contract> list = processContract.getContractsByClient(client);
-
-		if(client==null) System.out.println("Client is null");
-		if(list==null) System.out.println("List is null");
-		if(service==null) System.out.println("Service is null");
-		if(client!=null) System.out.println("Client biz name is "+client.getBusinessName());
-		if(contract!=null) System.out.println("Contract biz name is "+contract.getBusinessName());
-		
-		service.setClientID(client.getID());
-		service.setContractID(contract.getID());
-		
-		database.connect();
-		database.insert(service);
-		database.disconnect();
-		
-		if(service!=null) System.out.println("service biz name is "+service.getClientID()+" for client "+client.getID());
-		
+	public void testGetYearRevenueForClient() 
+	{
 		MonthReport report = new MonthReport(new Date(), 100.0);
 		MonthReport actual = financialRecords.getYearRevenueForClient(client).get(0);
 
 		System.out.println(actual.getValue());
+		System.out.println(report.getValue());		
+		
+		assertTrue("Client revenue does not match expected", report.getValue() == actual.getValue());
+	}
+
+	@Test
+	public void testGetYearRevenueForService()
+	{		
+		MonthReport report = new MonthReport(new Date(), 50.0);
+		MonthReport actual = financialRecords.getYearRevenueForService(serviceType).get(0);
+
+		System.out.println(actual.getValue());
 		System.out.println(report.getValue());
 		
-		assertEquals(report, actual);
+		assertTrue("Service revenue does not match expected", report.getValue() == actual.getValue());
+	}
+
+	@Test
+	public void testGetYearExpenseForService() 
+	{
+		ProcessExpenses processExpense = new ProcessExpenses(); 
 		
-		database.connect();
-		database.drop(contract);
-		database.drop(serviceType);
-		database.drop(client);
-		database.drop(service);
-		database.disconnect();
+		assertTrue("Expense not inserted", processExpense.insertExpense(new Expense(service.getID(), "Sup", 100.0, "Some expense", new Date())));		
 		
+		MonthReport report = new MonthReport(new Date(), 100.0);
+		MonthReport actual = financialRecords.getYearExpenseForService(serviceType).get(0);
+
+		System.out.println(actual.getValue());
+		System.out.println(report.getValue());
 		
+		assertTrue("Service revenue does not match expected", report.getValue() == actual.getValue());
+	}
+
+	@Test
+	public void testCalcClientRevenueToDate() 
+	{
+		double revenue = financialRecords.calcClientRevenueToDate(client);
+		assertTrue("Client revenue does not match", 100.0 == revenue);
+	}
+
+	@Test
+	public void testCalcClientExpensesToDate()
+	{
+		ProcessExpenses processExpense = new ProcessExpenses();
+		double expense;
 		
+		assertTrue("Expense not inserted", processExpense.insertExpense(new Expense(service.getID(), "Sup", 100.0, "Some expense", new Date())));		
+		
+		expense = financialRecords.calcClientRevenueToDate(client);
+		assertTrue("Client expenses does not match", 100.0 == expense);
 	}
 
 	@Test
-	public void testGetYearRevenueForService() {
-		fail("Not yet implemented");
+	public void testCalcServiceRevenueToDate() 
+	{
+		double revenue = financialRecords.calcServiceRevenueToDate(serviceType);
+		assertTrue("Service revenue does not match", 50.0 == revenue);
 	}
 
 	@Test
-	public void testGetYearExpenseForService() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCalcClientRevenueToDate() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCalcClientExpensesToDate() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCalcServiceRevenueToDate() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCalcServiceExpensesToDate() {
-		fail("Not yet implemented");
+	public void testCalcServiceExpensesToDate() 
+	{
+		ProcessExpenses processExpense = new ProcessExpenses();
+		double expense;
+		
+		assertTrue("Expense not inserted", processExpense.insertExpense(new Expense(service.getID(), "Sup", 100.0, "Some expense", new Date())));		
+		
+		expense = financialRecords.calcServiceExpensesToDate(serviceType);
+		assertTrue("Service expenses does not match", 100.0 == expense);
 	}
 
 }
